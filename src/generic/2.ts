d5459bb87e1fba8d7c2840736335e86cdf11fe2b
martadelka <oamedvedeva777@gmail.com>
2023-12-10 21:59:57 +0200

    fixed tasks

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -11,13 +11,16 @@ type AllType = {
   weight: number
 }
 
-function compare<T extends AllType, U extends AllType>(top: Pick<T, keyof AllType>, bottom: Pick<U, keyof AllType>): AllType {
+function compare<
+T extends Pick<AllType, "name" | "color">,
+U extends Pick<AllType, "position" | "weight">
+>(top: T, bottom: U): AllType { 
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
   };
-}
+ }
 
 export {};
